eba298bbd00ae340809394a647283c12a88b520a
RJToky <tokyrakotoarivony5@gmail.com>
2023-07-28 12:27:27 +0300

    Init commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,116 @@
+<?php $this->load->view("templates/header"); ?>
+<div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;">
+        <div class="owl-carousel header-carousel position-relative owl-loaded owl-drag">
+            
+            
+            
+        <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-2846px, 0px, 0px); transition: all 1.5s ease 0s; width: 9961px;"><div class="owl-item cloned" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-2.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-2.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item cloned" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-3.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-3.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item active" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-1.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-1.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-2.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-2.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-3.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-3.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item cloned" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-1.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-1.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div><div class="owl-item cloned" style="width: 1423px;"><div class="owl-carousel-item position-relative" data-dot="<img src='<?= base_url("assets/") ?>img/carousel-2.jpg'>">
+                <img class="img-fluid" src="<?= base_url("assets/") ?>img/carousel-2.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-10 col-lg-8">
+                                <h1 class="display-2 text-white  slideInDown">Pioneers Of Solar And Renewable Energy</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                                <a href="" class="btn btn-primary rounded-pill py-3 px-5  slideInLeft">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div></div></div>
+            <div class="owl-nav disabled">
+              <div class="owl-prev">prev</div>
+              <div class="owl-next">next</div>
+            </div>
+            <div class="owl-dots">
+              <div class="owl-dot active">
+                <img src='<?= base_url("assets/") ?>img/carousel-1.jpg'>
+              </div>
+              <div class="owl-dot">
+                <img src='<?= base_url("assets/") ?>img/carousel-2.jpg'>
+              </div>
+              <div class="owl-dot">
+                <img src='<?= base_url("assets/") ?>img/carousel-3.jpg">
+              </div>
+            </div>
+          </div>
+    </div>
+<?php $this->load->view("templates/footer"); ?>
