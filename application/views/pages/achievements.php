47c7de01f5bba81bc6ad98d445d968b29e3b4ba3
RAKOTOARIVONY Judichael Tokiniaina <tokyrakotoarivony5@gmail.com>
2023-08-04 20:14:46 +0300

    Add paginate

--- application/views/pages/achievements.php
+++ application/views/pages/achievements.php
@@ -54,6 +54,31 @@
                 </div>
             <?php } ?>
         </div>
+
+        <hr />
+
+        <div class="row mt-5">
+            <nav>
+                <ul class="pagination justify-content-center pagination-lg">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <p class="fw-bold text-center">Résultats : 12</p>
+        </div>
     </div>
 </div>
 <?php $this->load->view("templates/footer"); ?>
