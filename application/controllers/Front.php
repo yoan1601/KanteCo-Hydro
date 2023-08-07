adb1511c4648daec9b35df1b6e3ee98485156372
RAKOTOARIVONY Judichael Tokiniaina <tokyrakotoarivony5@gmail.com>
2023-08-07 11:23:33 +0300

    [ADD] Front sign_in

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -1,32 +1,36 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Front extends CI_Controller {
+class Front extends CI_Controller
+{
 
-	public function index() {
+	public function index()
+	{
 		redirect('front/home');
 	}
 
-	public function home() {
-		if($this->session->has_userdata('lang') == false) {
+	public function home()
+	{
+		if ($this->session->has_userdata('lang') == false) {
 			$this->session->set_userdata('lang', 'fr');
 		}
-		
+
 		$lang = $this->session->lang;
 
 		$data = $this->data->getData();
 
 		$data['lang'] = $lang;
-		$data['page'] = 'home'; 
+		$data['page'] = 'home';
 
 		$this->load->view('pages/home', ['data' => $data]);
 	}
 
-	public function reference() {
-		if($this->session->has_userdata('lang') == false) {
+	public function reference()
+	{
+		if ($this->session->has_userdata('lang') == false) {
 			$this->session->set_userdata('lang', 'fr');
 		}
-		
+
 		$lang = $this->session->lang;
 
 		$data = $this->data->getData();
@@ -39,32 +43,48 @@ class Front extends CI_Controller {
 		$this->load->view('pages/reference', ['data' => $data]);
 	}
 
-	public function achievements() {
-		if($this->session->has_userdata('lang') == false) {
+	public function achievements()
+	{
+		if ($this->session->has_userdata('lang') == false) {
 			$this->session->set_userdata('lang', 'fr');
 		}
-		
+
 		$lang = $this->session->lang;
 
 		$data = $this->data->getData();
 
 		$data['lang'] = $lang;
-		$data['page'] = 'achievements'; 
+		$data['page'] = 'achievements';
 		$this->load->view('pages/achievements', ['data' => $data]);
 	}
 
-	public function contact() {
-		if($this->session->has_userdata('lang') == false) {
+	public function contact()
+	{
+		if ($this->session->has_userdata('lang') == false) {
 			$this->session->set_userdata('lang', 'fr');
 		}
-		
+
 		$lang = $this->session->lang;
 
 		$data = $this->data->getData();
 
 		$data['lang'] = $lang;
-		$data['page'] = 'contact'; 
+		$data['page'] = 'contact';
 		$this->load->view('pages/contact', ['data' => $data]);
 	}
 
+	public function sign_in()
+	{
+		if ($this->session->has_userdata('lang') == false) {
+			$this->session->set_userdata('lang', 'fr');
+		}
+
+		$lang = $this->session->lang;
+
+		$data = $this->data->getData();
+
+		$data['lang'] = $lang;
+		$data['page'] = 'sign_in';
+		$this->load->view('pages/sign_in', ['data' => $data]);
+	}
 }
