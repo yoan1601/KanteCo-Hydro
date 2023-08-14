5c7873beaab60c986eee00732442c1003b258bbf
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-08-14 11:46:59 +0300

    dynamisation details_blog

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -284,7 +284,7 @@ class Front extends CI_Controller
 		$this->load->view('pages/devis', ['data' => $data]);
 	}
 
-	public function detail_blog()
+	public function detail_blog($id = 1)
 	{
 		if ($this->session->has_userdata('lang') == false) {
 			$this->session->set_userdata('lang', 'fr');
@@ -295,7 +295,33 @@ class Front extends CI_Controller
 		$data = $this->data->getData();
 
 		$data['lang'] = $lang;
-		$data['page'] = 'blog';
+
+		$data['blog'] = $this->blog->getById($id);
+		$data['blog'] = $this->blog->setAllImages_one_blog($data['blog']);
+
+				// ========== FORMATTAGE DATE ==========
+		// Créer un objet DateTime à partir de la date MySQL
+		$date = new DateTime($data['blog']['date_publication']);
+
+		// Formater la date dans le format '01 juillet 2023'
+		$formatted_date_EN = $date->format('d F Y');
+
+		$data['blog']['date_publication_formatted_EN'] = $formatted_date_EN;
+
+		// Définir la locale en français
+		$locale = 'fr_FR';
+
+		// Créer un objet IntlDateFormatter pour formater la date en français
+		$dateFormatter = new IntlDateFormatter($locale, IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+
+		// Formater la date
+		$formatted_date_FR = $dateFormatter->format($date);
+
+		$data['blog']['date_publication_formatted_FR'] = $formatted_date_FR;
+
+		// ========== FIN FORMATTAGE DATE ==========
+
+		$data['page'] = 'detail_blog-' . $id;
 
 		if ($this->session->has_userdata('user') == false) {
 			$data['session'] = false;
