1fe5033cbf7bb59e12223ba8f3070686e0ff56a1
RAKOTOARIVONY Judichael Tokiniaina <tokyrakotoarivony5@gmail.com>
2023-08-14 14:43:22 +0300

    front login admin

---
+++ application/controllers/AdministrationHydroGroup.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class AdministrationHydroGroup extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        redirect('AdministrationHydroGroup/login');
+    }
+
+    public function login()
+    {
+        $this->load->view('admin/login');
+    }
+}
